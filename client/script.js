b6a7140ecfedfea251d4b417d90a137bdecb96a4
mlsniperpro <algopro.km@gmail.com>
2023-01-01 03:26:34 -0500

    Update the localhost to render hosted server

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
    loader(messageDiv);
 
    //fetch data from server
-   const response = await fetch("http://localhost:5000/", {
+   const response = await fetch("https://chatgpt-clone-du4m.onrender.com", {
      method: "POST",
      headers: {
        "Content-Type": "application/json",
